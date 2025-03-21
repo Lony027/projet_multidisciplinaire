770c133d417dc9e237b42c365217f6d807905c58
Lony027 <iony@protonmail.com>
2025-03-21 12:25:58 +0100

    wip

---
+++ geolocate/geolocate.py
@@ -0,0 +1,15 @@
+import asyncio
+import nominatim_api as napi
+
+# python3 -m pip install nominatim-api
+# python3 -m pip install packaging/nominatim-api
+
+async def search(query):
+    async with napi.NominatimAPIAsync() as api:
+        return await api.search(query)
+
+results = asyncio.run(search('Brugge'))
+if not results:
+    print('Cannot find Brugge')
+else:
+    print(f'Found a place at {results[0].centroid.x},{results[0].centroid.y}')
