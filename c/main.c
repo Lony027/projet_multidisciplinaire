ec6c4336855e55c8659a1d601ae89457dce60d66
Dylan LANNUZEL <lannuzeld@gmail.com>
2025-03-28 12:04:45 +0100

    feat: Test matrice module function

--- c/main.c
+++ c/main.c
@@ -1,11 +1,11 @@
 #include "src/matrix.h"
 
-int main() {
+int test_matrice_module() {
     Matrix *m = open_csv_matrix("../mock_distance.csv");
     print_matrix(m);
 
     free_matrix(m);
 
-    return 0;
+    return 1;
 }
 
